6448f8d8252d064682296c7ab72e90cf17b1bcf4
Tomas Kovalik <tomas.kovalik@betsys.com>
2025-08-18 12:02:53 +0200

    day 3 part 2 solved... yay

--- 2023/python/puzzle_3.py
+++ 2023/python/puzzle_3.py
@@ -1,6 +1,5 @@
 # https://www.reddit.com/r/adventofcode/comments/189q9wv/2023_day_3_another_sample_grid_to_use/
 
-from curses.ascii import isdigit
 import math
 import re
 
@@ -44,6 +43,7 @@ def puzzle_3(filepath):
 
         return sum(part_numbers)
 
+
 def puzzle_3_refactored(filepath):
     part_numbers = []
 
@@ -53,14 +53,14 @@ def puzzle_3_refactored(filepath):
             for match in re.finditer(r"\d+", line):
                 num = int(match.group())
                 start = match.start()
-                end = match.end()-1
+                end = match.end() - 1
 
                 is_part = False
 
-                for r in range(max(0, i-1), min(len(lines), i+2)):
-                    for c in range(max(0, start-1), min(len(lines[r]), end+2)):
+                for r in range(max(0, i - 1), min(len(lines), i + 2)):
+                    for c in range(max(0, start - 1), min(len(lines[r]), end + 2)):
                         char = lines[r][c]
-                        if char not in '.1234567890':
+                        if char not in ".1234567890":
                             is_part = True
                             break
                     if is_part:
@@ -78,17 +78,16 @@ def puzzle_3b(filepath):
     with open(filepath) as f:
         lines = [line.strip() for line in f.readlines()]
         for i, line in enumerate(lines):
-
             # print("search for gears", line)
 
             for match in re.finditer(r"\*", line):
                 start = match.start()
-                end = match.end()-1
+                end = match.end() - 1
 
                 part_numbers = []
 
                 # check adjacent rows
-                for row in range(max(0, i-1), min(len(lines), i+2)):
+                for row in range(max(0, i - 1), min(len(lines), i + 2)):
                     # find all numbers in the row
                     numbers = list(re.finditer(r"\d+", lines[row]))
                     # print("  -> (search for part nums) processing row:", lines[row])
@@ -106,8 +105,45 @@ def puzzle_3b(filepath):
     return sum(gear_ratios)
 
 
+def puzzle_3b_refactored(filepath):
+    gear_ratios = []
+
+    with open(filepath) as f:
+        lines = [line.strip() for line in f.readlines()]
+
+        for i, line in enumerate(lines):
+            for gear in re.finditer(r"\*", line):
+                # print("gear found - line:", line)
+                start, end = gear.span()
+                part_numbers = []
+
+                for row in range(max(0, i - 1), min(len(lines), i + 2)):
+                    # print("  -> search for adjacent numbers:", lines[row])
+                    valid_nums = set()
+                    for char in range(
+                        max(0, start - 1), min(len(lines[row]), end + 1)
+                    ):  # end is inclusive so +1 is enough
+                        if lines[row][char] in "1234567890":
+                            # print("    -> adjacent number found:", lines[row][char])
+                            numbers = re.finditer(r"\d+", lines[row])  # get numbers in given row
+                            for n in numbers:
+                                n_start = n.start()
+                                n_end = n.end()
+                                if char in range(n_start, n_end):
+                                    valid_nums.add((int(n.group()), n.span()))
+                    # print("      -> valid numbers:", valid_nums)
+                    part_numbers.extend([tup[0] for tup in valid_nums])
+                    # print(part_numbers)
+
+                if len(part_numbers) == 2:
+                    gear_ratios.append(math.prod(part_numbers))
+                    # print(gear_ratios)
+
+    return sum(gear_ratios)
+
+
 if __name__ == "__main__":
-    #print(puzzle_3("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt"))
-    print(puzzle_3_refactored("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt")) # 539590
-    print(puzzle_3b("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt")) # 70890829
-                                                                                          # 79389556 #87263515
+    # print(puzzle_3("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt"))
+    print(puzzle_3_refactored("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt"))  # 539590
+    # print(puzzle_3b("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt")) # 70890829 # 79389556 #87263515
+    print(puzzle_3b_refactored("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_3.txt")) # 80703636
