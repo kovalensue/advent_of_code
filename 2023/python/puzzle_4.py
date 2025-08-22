d6ffdb2f75ed37804ac87bcfd7bd7515319216cd
Tomas Kovalik <tomas.kovalik@betsys.com>
2025-08-22 15:56:35 +0200

    day 4 2023 pyhton kick off

---
+++ 2023/python/puzzle_4.py
@@ -0,0 +1,9 @@
+def puzzle_4(filepath):
+    pass
+
+def puzzle_4b(filepath):
+    pass
+
+if __name__ == "__main__":
+    print(puzzle_4("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_4.txt"))
+    print(puzzle_4b("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_4.txt"))
