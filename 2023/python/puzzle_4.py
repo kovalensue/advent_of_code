7c98cdde624bbe98700fda4fdefd88fe655a5e92
kovalensue <tomas.kovalik@gmail.com>
2025-08-29 00:22:04 +0200

    puzzle 4 python first steps

--- 2023/python/puzzle_4.py
+++ 2023/python/puzzle_4.py
@@ -1,9 +1,20 @@
+from regex import P
+
+
 def puzzle_4(filepath):
-    pass
+    with open(filepath, "r") as f:
+        for line in f:
+            line = line.strip()
+            numbers = line.split(":")[1].strip()
+            print(numbers)
+
+    return "TBD"
+
 
 def puzzle_4b(filepath):
     pass
 
+
 if __name__ == "__main__":
-    print(puzzle_4("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_4.txt"))
-    print(puzzle_4b("/home/kovalikt/git/personal/advent_of_code/resources/puzzle_4.txt"))
+    print(puzzle_4("./resources/puzzle_4.txt"))
+    print(puzzle_4b("./resources/puzzle_4.txt"))
