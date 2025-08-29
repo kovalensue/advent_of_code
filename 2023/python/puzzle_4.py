80c15f1cd87e676367ad2bcb63741083f5ad893d
Tomas Kovalik <tomas.kovalik@betsys.com>
2025-08-29 15:56:22 +0200

    day 4 part 1 python

--- 2023/python/puzzle_4.py
+++ 2023/python/puzzle_4.py
@@ -1,14 +1,19 @@
-from regex import P
+from math import floor
 
 
 def puzzle_4(filepath):
+    points = []
+
     with open(filepath, "r") as f:
         for line in f:
             line = line.strip()
-            numbers = line.split(":")[1].strip()
-            print(numbers)
+            numbers = line.split(":")[1].strip().split("|")
+            winning_numbers = set([int(x) for x in numbers[0].strip().split(" ") if x])
+            played_numbers = set([int(x) for x in numbers[1].strip().split(" ") if x])
+            match_cnt = len(played_numbers.intersection(winning_numbers))
+            points.append(floor(pow(2, match_cnt-1)))
 
-    return "TBD"
+    return sum(points)
 
 
 def puzzle_4b(filepath):
@@ -16,5 +21,5 @@ def puzzle_4b(filepath):
 
 
 if __name__ == "__main__":
-    print(puzzle_4("./resources/puzzle_4.txt"))
+    print(puzzle_4("./resources/puzzle_4.txt")) # 21213
     print(puzzle_4b("./resources/puzzle_4.txt"))
