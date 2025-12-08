1840f57994067b6168c9c55a0eebdd56f984c4f7
kovalensue <tomas.kovalik@gmail.com>
2025-12-08 23:25:19 +0100

    2025 day 1 part 2

--- 2025/python/puzzle_1.py
+++ 2025/python/puzzle_1.py
@@ -1,9 +1,4 @@
-
-from hmac import new
-
-
 def puzzle_1(file_path):
-
     pos = 50
     psw = 0
 
@@ -18,27 +13,28 @@ def puzzle_1(file_path):
                 psw += 1
     return psw
 
-def puzzle_1b(file_path):
 
+def puzzle_1b(file_path):
+    N = 100
     pos = 50
-    psw = 0
+    wraps = 0
 
     with open(file_path) as f:
         for line in f:
             line = line.strip()
-            print(pos%100, pos, )
+            old_pos = pos
             if line.startswith("R"):
                 pos = pos + int(line[1:])
+                arr = [(x+1)%N for x in list(range(old_pos, pos, 1))]
             elif line.startswith("L"):
                 pos = pos - int(line[1:])
+                arr = [(x-1)%N for x in list(range(old_pos, pos, -1))]
 
+            wraps += arr.count(0)
 
-            if (pos%100)-pos == 100:
-                psw += 1
-
-    return psw
+    return wraps
 
 
 if __name__ == "__main__":
     print("Puzzle 1 - password is:", puzzle_1("2025/resources/puzzle_1.txt"))
-    print("Puzzle 1b - password is:", puzzle_1b("2025/resources/puzzle_1_test.txt"))
+    print("Puzzle 1b - password is:", puzzle_1b("2025/resources/puzzle_1.txt"))
