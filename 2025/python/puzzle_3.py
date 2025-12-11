fb8c6074bc6656ff8d617e45c72cb75d57a5a913
Tomas Kovalik <tomas.kovalik@betsys.com>
2025-12-11 18:36:15 +0100

    2025 Day 3 Puzzle 2

--- 2025/python/puzzle_3.py
+++ 2025/python/puzzle_3.py
@@ -1,19 +1,20 @@
-import heapq
 from itertools import combinations
 
+
 # nice but wrong :D (i didn't read the puzzle description correctly)
-def puzzle_3_z(filepath):
-    joltages = []
+# def puzzle_3_z(filepath):
+#     joltages = []
 
-    with open(filepath) as f:
-        for line in f:
-            batteries_indexed = [(idx, int(x)) for idx, x in enumerate(line.strip())]
-            top_indices = sorted([idx for idx, x in heapq.nlargest(2, batteries_indexed, key=lambda x: x[1])])
-            battery_joltages = [line[x] for x in top_indices]
-            joltage = ''.join(battery_joltages)
-            joltages.append(int(joltage))
+#     with open(filepath) as f:
+#         for line in f:
+#             batteries_indexed = [(idx, int(x)) for idx, x in enumerate(line.strip())]
+#             top_indices = sorted([idx for idx, x in heapq.nlargest(2, batteries_indexed, key=lambda x: x[1])])
+#             battery_joltages = [line[x] for x in top_indices]
+#             joltage = "".join(battery_joltages)
+#             joltages.append(int(joltage))
 
-    return sum(joltages)
+#     return sum(joltages)
+
 
 def puzzle_3(filepath):
     joltages = []
@@ -23,19 +24,39 @@ def puzzle_3(filepath):
             line = line.strip()
             batteries = [x for x in line]
             pairs = list(combinations(batteries, 2))
-            possible_bank_joltages = [int(''.join(pair)) for pair in pairs]
+            possible_bank_joltages = [int("".join(pair)) for pair in pairs]
             max_possible_bank_joltage = max(possible_bank_joltages)
             joltages.append(max_possible_bank_joltage)
 
     return sum(joltages)
 
 
-
 def puzzle_3b(filepath):
-    pass
+    joltages = []
+    NUM_LEN = 12
 
+    with open(filepath) as f:
+        for line in f:
+            digits = [int(x) for x in line.strip()]
+            to_remove = len(digits) - NUM_LEN
+            res = []
+
+            for digit in digits:
+                while len(res) > 0 and digit > res[-1] and to_remove > 0:
+                    res.pop()
+                    to_remove -= 1
+                res.append(digit)
+
+            while to_remove > 0:
+                res.pop()
+                to_remove -= 1
+
+            joltage = int("".join(str(x) for x in res))
+            joltages.append(joltage)
+
+    return sum(joltages)
 
 
 if __name__ == "__main__":
     print("Puzzle 2 - total joltage is:", puzzle_3("2025/resources/puzzle_3.txt"))
-    #print("Puzzle 1b - sum of invalid IDs is:", puzzle_3b("2025/resources/puzzle_3.txt"))
+    print("Puzzle 2b - total joltage is:", puzzle_3b("2025/resources/puzzle_3.txt"))
